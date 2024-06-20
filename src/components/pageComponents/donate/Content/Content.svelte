b0644154475b9fa2976db5ac249d3effb0316632
Lara <lovemirror@rambler.ru>
2024-06-20 22:38:49 +0300

    Add content to donate

--- src/components/pageComponents/donate/Content/Content.svelte
+++ src/components/pageComponents/donate/Content/Content.svelte
@@ -1,18 +1,34 @@
 <div class="wrapper">
-    <div>Поддержать проект можно рублем</div>
-    <div>СБП "Сбербанк"</div>
-    <div>Роман Мергеевич М.</div>
-    <div>+7 911 999 89 33</div>
-    <div>Скопировать номер телефона</div>
-    <div>2202206314670147</div>
-    <div>Скопировать номер карты</div>
-    <div>Просьба писать в комментарии к переводу "на развитие Snake Way"</div>
+    <div class="header-text">Поддержать проект можно рублем</div>
+    <div class="header-text">СБП "Сбербанк"<br />Роман Сергеевич М.</div>
+    <div class="copy-text">+7 911 999 89 33<br />скопировать номер телефона</div>
+    <div class="copy-text">2202206314670147<br />скопировать номер карты</div>
+    <div class="comment-text">Просьба писать в комментарии к переводу<br />"на развитие Snake Way"</div>
 </div>
 
 <style>
     .wrapper {
         display: flex;
         flex-direction: column;
+        align-items: center;
         gap: 10px;
+        font-family: 'Century Gothic';
+    }
+    .header-text {
+      text-align: center;
+      font-size: 16px;
+    }
+    .comment-text {
+      text-align: center;
+      font-weight: bold;
+      font-size: 10px;
+    }
+    .copy-text {
+      text-align: center;
+      box-sizing: border-box;
+      padding: 3px 10px;
+      border: 10px solid lightgrey;
+      background-color: darkgray;
+      font-size: 12px;
     }
 </style>
