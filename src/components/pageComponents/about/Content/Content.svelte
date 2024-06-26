5a72fd44da9f12325ed3148b74b24256d38f564f
Lara <lovemirror@rambler.ru>
2024-06-26 22:27:23 +0300

    Add main page styles

--- src/components/pageComponents/about/Content/Content.svelte
+++ src/components/pageComponents/about/Content/Content.svelte
@@ -1,16 +1,35 @@
 <script>
+	import IconLinkButton from "../../../atoms/IconLinkButton/IconLinkButton.svelte";
 	import LinkButton from "../../../atoms/LinkButton/LinkButton.svelte";
-
+  import Logo from './icons/logo.svelte';
+  import InstLogo from './icons/instLogo.svelte';
+  import MbLogo from './icons/mbLogo.svelte';
+  import T13Logo from './icons/t13Logo.svelte';
+  import TgLogo from './icons/tgLogo.svelte';
+  import VkLogo from './icons/vkLogo.svelte';
 </script>
 
 <div class="wrapper">
-    <div>Logo</div>
+    <div class="logo-wrapper">
+      <Logo />
+    </div>
+
     <div class="buttons-wrapper">
-      <LinkButton>TG</LinkButton>
-      <LinkButton>VK</LinkButton>
-      <LinkButton>Inst</LinkButton>
-      <LinkButton>MB</LinkButton>
-      <LinkButton>XZ</LinkButton>
+      <IconLinkButton>
+        <TgLogo />
+      </IconLinkButton>
+      <IconLinkButton>
+        <VkLogo />
+      </IconLinkButton>
+      <IconLinkButton>
+        <InstLogo />
+      </IconLinkButton>
+      <IconLinkButton>
+        <MbLogo />
+      </IconLinkButton>
+      <IconLinkButton>
+        <T13Logo />
+      </IconLinkButton>
     </div>
     <div class="buttons-wrapper">
       <LinkButton href="/shop">Shop</LinkButton>
@@ -20,12 +39,20 @@
 
 <style>
     .wrapper {
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 20px;
+      height: 100%;
+    }
+    .logo-wrapper {
+      width: 436px;
+      height: 436px;
     }
     .buttons-wrapper {
       display: flex;
-      gap: 5px;
+      width: 355px;
+      justify-content: space-between;
     }
 </style>
