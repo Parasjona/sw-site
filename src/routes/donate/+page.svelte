e93a3287f6830c230287643d1a0224fd00f633d4
Lara <lovemirror@rambler.ru>
2024-06-26 22:36:40 +0300

    Fix code format

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { Content } from "../../components/pageComponents/donate/Content";
-    import { Main } from "../../components/templates/Main";
+  import { Main } from "../../components/templates/Main";
 </script>
 
 <Main>
