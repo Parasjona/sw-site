e8d864b907f519e2702a7f6b23eced84bc90826a
Lara <lovemirror@rambler.ru>
2024-06-27 00:51:49 +0300

    Set deploy

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let data;
+</script>
+
+<div class="main-container-wrapper" >
+  {#key data.pathname}
+  <div class="main-container">
+    <slot />
+  </div>
+  {/key}
+</div>
