db89dff432f9de8ebf14a829e71467bc056e5538
Lara <lovemirror@rambler.ru>
2024-06-26 22:27:06 +0300

    Fix LinkButton styles

--- src/components/atoms/LinkButton/LinkButton.svelte
+++ src/components/atoms/LinkButton/LinkButton.svelte
@@ -3,23 +3,42 @@
 </script>
 
 <a {...$$restProps} {href}>
-  <slot />
+  <div>
+    <slot />
+  </div>
 </a>
 
 <style>
   a {
+    all: unset;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
     cursor: pointer;
     box-sizing: border-box;
     text-align: center;
-    height: 30px;
-    padding: 2px;
+    width: 168px;
+    height: 57px;
 
-    border: 2px solid black;
-    border-radius: 7px;
+    border: 3px solid black;
+    border-radius: 17px;
 
-    background-color: yellow;
-    color: red;
+    background-color: rgb(243, 219, 9);
+    color: rgb(204, 0, 0);
 
     font-family: 'Texas Tango';
+    font-size: 32px;
+  }
+  a:after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    border: 3px solid rgb(204, 153, 0);
+    border-radius: 14px;
   }
 </style>
