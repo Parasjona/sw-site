05dc3f4e34654cd67b98505c1a24c1dfeb03a849
Lara <lovemirror@rambler.ru>
2024-06-29 19:20:03 +0300

    Add photos for shop

--- src/components/molecules/ProductCard/ProductCard.svelte
+++ src/components/molecules/ProductCard/ProductCard.svelte
@@ -2,10 +2,21 @@
 	export let name: string;
 	export let category: string = 'Snake Way Division';
 	export let price: string;
+	export let photo: string = undefined;
+	import { base } from '$app/paths';
+
+	const srcUrl = `${base}/images/products/${photo}`;
 </script>
 
 <div class="product-card">
-	<div class="product-card-photo-block"></div>
+	<div class="product-card-photo-block">
+		{#if photo}
+			<img class="avatar" src={srcUrl} alt={name} />
+			<div class="product-card-photo-block-slideline"></div>
+		{:else}
+			<div class="product-card-text">Товар на фотосессии</div>
+		{/if}
+	</div>
 	<div class="product-card-text">{name}</div>
 	<div class="product-card-text">{category}</div>
 	<div class="product-card-text">{price}</div>
@@ -14,17 +25,30 @@
 
 <style>
 	.product-card {
-		width: 300px;
 		height: 375px;
 		background-color: var(--light-grey);
+		padding: 0;
 	}
 	.product-card-photo-block {
 		position: relative;
-		width: 300px;
-		height: 215px;
+		width: 293px;
+		height: 210px;
+		overflow: hidden;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.product-card-photo-block-slideline {
+		position: absolute;
+		bottom: 0;
+		left: 0;
 		background-color: var(--yellow);
+		height: 10px;
+		width: 100%;
+		opacity: 0.7;
 	}
 	.product-card-text {
+		text-align: center;
 		font-size: 23px;
 	}
 	button {
--- src/components/pageComponents/shop/Content/Content.svelte
+++ src/components/pageComponents/shop/Content/Content.svelte
@@ -1,13 +1,29 @@
 <script>
 	import { ProductCard } from '@components/molecules/ProductCard';
+
+	const products = [
+		{
+			name: 'Стикерпак',
+			category: 'Snake Way Division',
+			price: '250р.',
+			photo: 'stickerpack1.png'
+		},
+		{
+			name: 'Брелок-ремувка',
+			category: 'Snake Way Division',
+			price: '400р.',
+			photo: 'keychain1.png'
+		},
+		{ name: 'Флаг 135х95', category: 'Snake Way Division', price: '1300р.', photo: 'flag1.png' },
+		{ name: 'Кепка', category: 'Snake Way Division', price: '2000р.' },
+		{ name: 'Футболка', category: 'Snake Way Division', price: '3000р.' }
+	];
 </script>
 
 <div class="wrapper">
-	<ProductCard name="Стикерпак" category="Snake Way Division" price="250р." />
-	<ProductCard name="Брелок-ремувка" category="Snake Way Division" price="400р." />
-	<ProductCard name="Флаг 135х95" category="Snake Way Division" price="1300р." />
-	<ProductCard name="Кепка" category="Snake Way Division" price="2000р." />
-	<ProductCard name="Футболка" category="Snake Way Division" price="3000р." />
+	{#each products as item}
+		<ProductCard {...item} />
+	{/each}
 </div>
 
 <style>
