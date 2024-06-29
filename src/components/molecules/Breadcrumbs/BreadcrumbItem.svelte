11c613925ce68748e342796f8b4a963086570c3f
Lara <lovemirror@rambler.ru>
2024-06-29 16:52:27 +0300

    Fix Breadcrumbs

--- src/components/molecules/Breadcrumbs/BreadcrumbItem.svelte
+++ src/components/molecules/Breadcrumbs/BreadcrumbItem.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
-	import ChevronRight from "./ChevronRight.svelte";
+	import ChevronRight from './ChevronRight.svelte';
 
-  export let isActive: boolean;
-  export let url: string;
+	export let isActive: boolean = undefined;
+	export let url: string = undefined;
 </script>
 
 <li class="breadcrumb">
-  {#if isActive}
-    <a class="content" href={url}>
-      <slot />
-        <ChevronRight />
-    </a>
-  {:else}
-    <span class="content">
-      <slot />
-    </span>
-  {/if}
+	{#if isActive}
+		<a class="content" href={url}>
+			<slot />
+			<ChevronRight />
+		</a>
+	{:else}
+		<span class="content">
+			<slot />
+		</span>
+	{/if}
 </li>
 
 <style>
-  .breadcrumb {
-    display: flex;
-    flex-shrink: 0;
-    align-items: center;
-    white-space: nowrap;
-  }
-  .content {
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    position: relative;
-    color: black;
-  }
+	.breadcrumb {
+		display: flex;
+		flex-shrink: 0;
+		align-items: center;
+		white-space: nowrap;
+	}
+	.content {
+		display: flex;
+		align-items: center;
+		text-decoration: none;
+		position: relative;
+		color: black;
+	}
 </style>
--- src/components/molecules/Breadcrumbs/Breadcrumbs.svelte
+++ src/components/molecules/Breadcrumbs/Breadcrumbs.svelte
@@ -1,56 +1,52 @@
-<script>
-  export let items;
-</script>
+<script></script>
 
 <nav aria-label="Breadcrumb" class="breadcrumbs">
-  <ol class="wrapper">
-    <slot />
-  </ol>
+	<ol class="wrapper">
+		<slot />
+	</ol>
 </nav>
 
 <style>
-  .breadcrumbs {
+	.breadcrumbs {
 		font-family: 'Century Gothic';
 		font-size: 30px;
-    position: relative;
-    box-sizing: border-box;
-    display: flex;
-    width: 100%;
-    padding: 20px 40px;
+		position: relative;
+		box-sizing: border-box;
+		display: flex;
+		width: 100%;
 
-    @media (max-width: 700px) {
+		@media (max-width: 700px) {
 			font-size: 15px;
-      padding: 10px 20px;
 		}
-  }
-  .wrapper {
-    position: relative;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-start;
-    width: 100%;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    flex-wrap: nowrap;
-    overflow-x: scroll;
-    overflow-y: visible;
-    &::-webkit-scrollbar {
-      width: 0 !important;
-      height: 0 !important;
-    }
-    overflow: -moz-scrollbars-none;
-    -ms-overflow-style: none;
-    scrollbar-width: none;
+	}
+	.wrapper {
+		position: relative;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: flex-start;
+		width: 100%;
+		list-style: none;
+		padding: 0;
+		margin: 0;
+		flex-wrap: nowrap;
+		overflow-x: scroll;
+		overflow-y: visible;
+		&::-webkit-scrollbar {
+			width: 0 !important;
+			height: 0 !important;
+		}
+		overflow: -moz-scrollbars-none;
+		-ms-overflow-style: none;
+		scrollbar-width: none;
 
-    & > li {
-      margin-left: 4px;
-    }
-    & > li:first-child,
-    & > li[data-overflow] {
-      margin-left: 0;
-    }
-  }
+		& > li {
+			margin-left: 4px;
+		}
+		& > li:first-child,
+		& > li[data-overflow] {
+			margin-left: 0;
+		}
+	}
 </style>
