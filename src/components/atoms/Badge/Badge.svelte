a8e48e369a2bd20d493806aae6d531d22d989046
Lara <lovemirror@rambler.ru>
2024-06-29 17:43:29 +0300

    Add Badge

---
+++ src/components/atoms/Badge/Badge.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<div class="badge">
+	<slot />
+</div>
+
+<style>
+	.badge {
+		font-family: 'Century Gothic Bold';
+		font-size: 20px;
+		border-radius: 50%;
+		background-color: var(--yellow);
+		padding: 0 7px;
+	}
+</style>
