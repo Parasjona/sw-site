2034c6d9e394253afd9f95f0e0c753d76e51eed5
Lara <lovemirror@rambler.ru>
2024-06-29 18:20:51 +0300

    Add product card

---
+++ src/components/molecules/ProductCard/ProductCard.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let name: string;
+	export let category: string = 'Snake Way Division';
+	export let price: string;
+</script>
+
+<div class="product-card">
+	<div class="product-card-photo-block"></div>
+	<div class="product-card-text">{name}</div>
+	<div class="product-card-text">{category}</div>
+	<div class="product-card-text">{price}</div>
+	<button>В КОРЗИНУ</button>
+</div>
+
+<style>
+	.product-card {
+		width: 300px;
+		height: 375px;
+		background-color: var(--light-grey);
+	}
+	.product-card-photo-block {
+		position: relative;
+		width: 300px;
+		height: 215px;
+		background-color: var(--yellow);
+	}
+	.product-card-text {
+		font-size: 23px;
+	}
+	button {
+		all: unset;
+		cursor: pointer;
+		width: 272px;
+		height: 58px;
+		background-color: var(--button-base-grey);
+		font-family: 'Century Gothic Bold';
+		font-size: 30px;
+	}
+</style>
