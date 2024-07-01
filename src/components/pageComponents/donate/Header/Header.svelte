42751c1c8d643b5a9ec2cf7f460dc88429d27504
Lara <lovemirror@rambler.ru>
2024-07-01 21:40:17 +0300

    Fix homeUrl

--- src/components/pageComponents/donate/Header/Header.svelte
+++ src/components/pageComponents/donate/Header/Header.svelte
@@ -1,11 +1,13 @@
 <script>
 	import { Breadcrumbs, BreadcrumbItem } from '@components/molecules/Breadcrumbs';
 	import { HeaderBlock } from '@components/templates/HeaderBlock';
+
+  const homeUrl = ".\\";
 </script>
 
 <HeaderBlock>
 	<Breadcrumbs>
-		<BreadcrumbItem isActive url=".\">Главная</BreadcrumbItem>
+		<BreadcrumbItem isActive url={homeUrl}>Главная</BreadcrumbItem>
 		<BreadcrumbItem>Donate</BreadcrumbItem>
 	</Breadcrumbs>
 </HeaderBlock>
--- src/components/pageComponents/shop/Header/Header.svelte
+++ src/components/pageComponents/shop/Header/Header.svelte
@@ -1,36 +1,38 @@
-<script>
+<script lang="ts">
 	import { Breadcrumbs, BreadcrumbItem } from '@components/molecules/Breadcrumbs';
 	import { HeaderBlock } from '@components/templates/HeaderBlock';
 	import { Badge } from '@components/atoms/Badge';
 	import ShoppingCart from './icons/shoppingCart.svelte';
 	import { count } from '../stores.ts';
+
+  const homeUrl = ".\\";
 </script>
 
 <HeaderBlock>
 	<Breadcrumbs>
-		<BreadcrumbItem isActive url=".\">Главная</BreadcrumbItem>
+		<BreadcrumbItem isActive url={homeUrl}>Главная</BreadcrumbItem>
 		<BreadcrumbItem>Shop</BreadcrumbItem>
 	</Breadcrumbs>
-	<div class="shoppping-cart">
-		<div class="shoppping-cart-icon">
-			<ShoppingCart />
+	<div class="shoping-cart">
+		<div class="shoping-cart-icon">
+			<ShopingCart />
 		</div>
 		<div class="badge-wrapper">
 			<Badge>
-				{$count}
+				{$cartQuantity}
 			</Badge>
 		</div>
 	</div>
 </HeaderBlock>
 
 <style>
-	.shoppping-cart {
+	.shoping-cart {
 		position: relative;
 		display: flex;
 		justify-content: flex-end;
 		box-sizing: border-box;
 	}
-	.shoppping-cart-icon {
+	.shoping-cart-icon {
 		height: 39px;
 		margin-top: 10px;
 	}
